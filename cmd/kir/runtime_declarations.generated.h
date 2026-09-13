294cc19f8de23a4b02dfada6fe3e374ea0fbc38c
Waozi <waozi@proton.me>
2026-09-13 04:57:29 -0300

    Move Text typeface selection to KSS

--- cmd/kir/runtime_declarations.generated.h
+++ cmd/kir/runtime_declarations.generated.h
@@ -1273,7 +1273,6 @@ static const struct { const char *path; const char *source; } runtime_sources[] 
         "    disabled: bool\n"
         "    letter_spacing: i32\n"
         "    selectable: bool\n"
-        "    typeface: const char*\n"
         "}\n"
     },
     {"runtime/title_bar_props.kry",
