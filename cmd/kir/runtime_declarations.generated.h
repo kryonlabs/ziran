4d7ccd5e3af59f828882995b15ec9a23b61b495a
Waozi <waozi@proton.me>
2026-09-18 18:01:09 -0300

    Fix modal action text and text props

--- cmd/kir/runtime_declarations.generated.h
+++ cmd/kir/runtime_declarations.generated.h
@@ -1383,6 +1383,8 @@ static const struct { const char *path; const char *source; } runtime_sources[] 
         "TextProps :: struct {\n"
         "    bounds: Rectangle\n"
         "    text: const char*\n"
+        "    font: i32\n"
+        "    color: Color\n"
         "    class_name: i32\n"
         "    wrap: TextWrap\n"
         "    align: TextAlign\n"
