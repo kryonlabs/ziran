9aedfadbb47cfd6c36f585fc7bb42d9ba927db24
Waozi <waozi@proton.me>
2026-09-19 11:39:16 -0300

    Use Unicode grapheme boundaries for native text editing

--- tests/k2go_syntax_test.sh
+++ tests/k2go_syntax_test.sh
@@ -679,6 +679,7 @@ module kryon-generated-smoke
 go 1.25.0
 
 require (
+	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/waozixyz/kryon/go/kryon v0.0.0
 	golang.org/x/image v0.45.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
